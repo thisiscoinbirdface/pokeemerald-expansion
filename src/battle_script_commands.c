83061bbdd56241a7254860401a71730d4f750ef8
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-03-02 22:52:59 +0530

    upd: battle speedup

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2973,7 +2973,7 @@ static void Cmd_waitmessage(void)
             u16 toWait = cmd->time;
             if (gTestRunnerHeadless)
                 gPauseCounterBattle = toWait;
-            if (++gPauseCounterBattle >= toWait)
+            if (++gPauseCounterBattle >= toWait || (JOY_NEW(A_BUTTON | B_BUTTON)))
             {
                 gPauseCounterBattle = 0;
                 gBattlescriptCurrInstr = cmd->nextInstr;
@@ -5755,7 +5755,7 @@ static void Cmd_pause(void)
         u16 value = cmd->frames;
         if (gTestRunnerHeadless)
             gPauseCounterBattle = value;
-        if (++gPauseCounterBattle >= value)
+        if (++gPauseCounterBattle >= value || (JOY_NEW(A_BUTTON | B_BUTTON)))
         {
             gPauseCounterBattle = 0;
             gBattlescriptCurrInstr = cmd->nextInstr;
