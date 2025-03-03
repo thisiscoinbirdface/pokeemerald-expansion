5dbe1065e09689b66d2cab43db0a642a3723e37a
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-03-03 11:25:46 +0530

    huge upd: fixes list + gastree layout 1

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1182,3 +1182,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/Route3/scripts.inc"
 
 	.include "data/maps/Route3_Underpass/scripts.inc"
+
+	.include "data/maps/GastreeCity/scripts.inc"
