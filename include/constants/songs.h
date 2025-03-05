eadad1299598c9ba19c0308046bdb1ec8c8863eb
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-03-06 00:55:34 +0530

    more music added

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -546,7 +546,9 @@
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
 
-#define MUS_GARDEN_MOON             610
+#define MUS_CUSTOM_GARDEN_MOON             610
+#define MUS_CUSTOM_JETLAG            610
+#define MUS_CUSTOM_FLOCKOFBIRDS             610
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
