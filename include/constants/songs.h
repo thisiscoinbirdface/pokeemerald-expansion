2c6aa897392319761c4d61b8dd71bc22ca537d18
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-03-06 00:50:40 +0530

    upd: music test

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -546,6 +546,8 @@
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
 
+#define MUS_GARDEN_MOON             610
+
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
 #define MUS_NONE                    0xFFFF
