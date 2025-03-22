842cc8fa3ba0f7193d3a401938b2227d416b3621
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-03-22 20:51:22 +0530

    upd: songs try

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -549,6 +549,8 @@
 #define MUS_CUSTOM_GARDEN_MOON             610
 #define MUS_CUSTOM_JETLAG            611
 #define MUS_CUSTOM_FLOCKOFBIRDS             612
+#define MUS_CUSTOM_SENSIBLE             613
+#define MUS_CUSTOM_LILAC             614
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
