0e31ff6c4378c1e1cb4570d81d5db883b5a14edd
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-03-29 18:59:48 +0530

    upd: greta done

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -161,7 +161,7 @@
 #define FLAG_HIDE_GASTREE_SKITTY1  0x2A3 // the first of the 3 skittys (trees)
 #define FLAG_HIDE_GASTREE_SKITTY2  0x2A4 // see above (teahouse garden)
 #define FLAG_HIDE_GASTREE_SKITTY3  0x2A5 // see above (below her trainer)
-#define FLAG_UNUSED_0x2A6  0x2A6 // Unused Flag
+#define FLAG_GRETA_ROUTE5  0x2A6 // encounter with Greta on R5 - random items
 #define FLAG_UNUSED_0x2A7  0x2A7 // Unused Flag
 #define FLAG_UNUSED_0x2A8  0x2A8 // Unused Flag
 #define FLAG_UNUSED_0x2A9  0x2A9 // Unused Flag
