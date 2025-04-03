3d81da88e58a0d6615fc2a7832d99b7894cb3f73
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-04-03 15:02:45 +0530

    tinypush

--- src/cable_car.c
+++ src/cable_car.c
@@ -792,7 +792,10 @@ static void CreateCableCarSprites(void)
 
     u16 playerGraphicsIds[2] = {
         [MALE]   = OBJ_EVENT_GFX_RIVAL_BRENDAN_NORMAL,
-        [FEMALE] = OBJ_EVENT_GFX_RIVAL_MAY_NORMAL
+        [FEMALE] = OBJ_EVENT_GFX_RIVAL_MAY_NORMAL,
+        [MUNUCU] = OBJ_EVENT_GFX_MUNUCU_NORMAL,
+        [SHUBUBU] = OBJ_EVENT_GFX_SHUBUBU_NORMAL,
+        [GUBUKING] = OBJ_EVENT_GFX_GUBUKING_NORMAL,                
     };
     u16 rval = Random();
     u16 hikerGraphicsIds[4] = {
