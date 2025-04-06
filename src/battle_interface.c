75563c7edcedb24389acf59b8c595c2808397277
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-04-06 10:40:02 +0530

    upd: battle hp fraction to 12

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -2068,7 +2068,7 @@ s32 MoveBattleBar(u8 battlerId, u8 healthboxSpriteId, u8 whichBar, u8 unused)
                     gBattleSpritesDataPtr->battleBars[battlerId].oldValue,
                     gBattleSpritesDataPtr->battleBars[battlerId].receivedValue,
                     &gBattleSpritesDataPtr->battleBars[battlerId].currValue,
-                    B_HEALTHBAR_PIXELS / 16, hpFraction);
+                    B_HEALTHBAR_PIXELS / 12, hpFraction);
     }
     else // exp bar
     {
