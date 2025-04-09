ab74fe58c5d71653fbdba963ed353fa7815c1231
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-04-09 10:32:12 +0530

    tinypush

--- src/caps.c
+++ src/caps.c
@@ -10,11 +10,11 @@ u32 GetCurrentLevelCap(void)
     static const u32 sLevelCapFlagMap[][2] =
     {
         {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 19},
-        {FLAG_BADGE03_GET, 24},
+        {FLAG_BADGE02_GET, 20},
+        {FLAG_BADGE03_GET, 26},
         {FLAG_BADGE04_GET, 29},
-        {FLAG_BADGE05_GET, 31},
-        {FLAG_BADGE06_GET, 33},
+        {FLAG_BADGE05_GET, 36},
+        {FLAG_BADGE06_GET, 39},
         {FLAG_BADGE07_GET, 42},
         {FLAG_BADGE08_GET, 46},
         {FLAG_IS_CHAMPION, 58},
