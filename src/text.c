f7cb33cc04f8e6ba0bc853d748f868aef1a1aedf
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-04-14 10:21:24 +0530

    upd: instant text

--- src/text.c
+++ src/text.c
@@ -400,10 +400,11 @@ void RunTextPrinters(void)
             if (sTextPrinters[i].active)
             {
                 u16 renderCmd = RenderFont(&sTextPrinters[i]);
+                CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, 2);
                 switch (renderCmd)
                 {
                 case RENDER_PRINT:
-                    CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, COPYWIN_GFX);
+                    CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, 2);
                 case RENDER_UPDATE:
                     if (sTextPrinters[i].callback != NULL)
                         sTextPrinters[i].callback(&sTextPrinters[i].printerTemplate, renderCmd);
@@ -1079,6 +1080,7 @@ static u16 RenderText(struct TextPrinter *textPrinter)
     u16 currChar;
     s32 width;
     s32 widthHelper;
+    u8 repeats = 4;
 
     switch (textPrinter->state)
     {
@@ -1102,8 +1104,23 @@ static u16 RenderText(struct TextPrinter *textPrinter)
         else
             textPrinter->delayCounter = textPrinter->textSpeed;
 
-        currChar = *textPrinter->printerTemplate.currentChar;
-        textPrinter->printerTemplate.currentChar++;
+        switch (GetPlayerTextSpeed())
+        {
+            case OPTIONS_TEXT_SPEED_SLOW:
+                repeats = 1;
+                break;
+            case OPTIONS_TEXT_SPEED_MID:
+                repeats = 2;
+                break;
+            case OPTIONS_TEXT_SPEED_FAST:
+                repeats = 4;
+                break;
+        }
+
+        do{
+     
+            currChar = *textPrinter->printerTemplate.currentChar;
+            textPrinter->printerTemplate.currentChar++;
 
         switch (currChar)
         {
@@ -1320,6 +1337,11 @@ static u16 RenderText(struct TextPrinter *textPrinter)
             else
                 textPrinter->printerTemplate.currentX += gCurGlyph.width;
         }
+
+        repeats --;
+
+      } while (repeats > 0);
+
         return RENDER_PRINT;
     case RENDER_STATE_WAIT:
         if (TextPrinterWait(textPrinter))
