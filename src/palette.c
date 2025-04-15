30856dc7cb40e8c841c79fceb410028c91669f2d
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-04-16 00:40:36 +0530

    upd

--- src/palette.c
+++ src/palette.c
@@ -33,11 +33,6 @@ EWRAM_DATA u8 ALIGNED(2) gPaletteDecompressionBuffer[PLTT_SIZE] = {0};
 
 static EWRAM_DATA u32 sPlttPreviousUpdateResult = 0; // Fast Battle Speed
 
-static const struct PaletteStructTemplate sDummyPaletteStructTemplate = {
-    .id = 0xFFFF,
-    .state = 1
-};
-
 static const u8 sRoundedDownGrayscaleMap[] = {
      0,  0,  0,  0,  0,
      5,  5,  5,  5,  5,
