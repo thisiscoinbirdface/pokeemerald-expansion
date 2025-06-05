f40e3c6aa8697bb2ad2584a19e5154a57770db18
thisiscoinbirdface <thisiscoinbirdface@hotmail.com>
2025-06-05 13:48:56 +0530

    update menu strings

--- src/start_menu.c
+++ src/start_menu.c
@@ -193,9 +193,9 @@ static const struct WindowTemplate sWindowTemplate_PyramidPeak = {
     .baseBlock = 0x8
 };
 
-static const u8 sText_MenuDebug[] = _("DEBUG");
+static const u8 sText_MenuDebug[] = _("Debug");
 
-static const u8 sText_QuestMenu[] = _("QUESTS");
+static const u8 sText_QuestMenu[] = _("Quests");
 static const struct MenuAction sStartMenuItems[] =
 {
     [MENU_ACTION_POKEDEX]           = {gText_MenuPokedex, {.u8_void = StartMenuPokedexCallback}},
